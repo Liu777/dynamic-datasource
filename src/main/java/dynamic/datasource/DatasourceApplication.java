052514bd243ab4fb775164225438031b21c64c33
liuqi <a18627830855@163.com>
2019-06-27 16:16:31 +0800

    使用springboot mybatis aop注解实现多数据源切换

---
+++ src/main/java/dynamic/datasource/DatasourceApplication.java
@@ -0,0 +1,15 @@
+package dynamic.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourceApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("wuxf.cn");
+        SpringApplication.run(DatasourceApplication.class, args);
+    }
+
+}
